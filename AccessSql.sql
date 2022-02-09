46c1c479410dd00c32ced3fa18f8dcfac9e03a42
Steve3099 <73429599+Steve3099@users.noreply.github.com>
2022-02-09 19:56:16 +0300

    Update AccessSql.sql

--- AccessSql.sql
+++ AccessSql.sql
@@ -1,5 +1,6 @@
 
 
-select id,label,amperage,voltage,
-int((iif(label='Panneaux solaire',10000/([voltage]*[amperage]),iif(label='baterie',5000/([voltage]*[amperage]))))+0.99)
+
+select id,label,amperage,voltage,price*util as totale,
+int((iif(label='Panneaux solaire',10000/([voltage]*[amperage]),iif(label='baterie',5000/([voltage]*[amperage]))))+0.99) as util
 from produit
