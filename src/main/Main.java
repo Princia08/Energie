961950539c5ba3e3cd210de4d8423a3737330346
Steve3099 <73429599+Steve3099@users.noreply.github.com>
2022-02-09 12:59:17 +0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,24 +6,43 @@ import java.util.List;
 import Interface.MonInterface;
 import appareil.Appareil;
 import service.Service;
+import java.sql.*;  
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//MonInterface mi=new MonInterface();
-		Service s = new Service();
+		/*Service s = new Service();
 		
 		Appareil a1=new Appareil();
 		a1.setHeureDebut("05:00");
 		a1.setHeureFin("22:00");
 		Appareil a2=new Appareil();
-		a2.setHeureDebut("23:00");
-		a2.setHeureFin("01:00");
+		a2.setHeureDebut("18:00");
+		a2.setHeureFin("06:00");
 		List<Appareil> lia = new ArrayList<Appareil>();
 		lia.add(a1);lia.add(a2);
 		
-		s.finale(lia);
+		s.finale(lia);*/
+		
+		
+		
+		 try{  
+		   String url="jdbc:ucanaccess://D://itu//S5//Mr Tahiana//Access//Energie.accdb";  
+		   //Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");  
+		   Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+		   Connection c=DriverManager.getConnection(url);  
+		   Statement st=c.createStatement();  
+		   ResultSet rs=st.executeQuery("select * from produit");  
+		    
+		   while(rs.next()){  
+		    System.out.println(rs.getString(1));  
+		   }  
+		  
+		}catch(Exception ee){System.out.println(ee);}  
+		  
+		
 	}
 
 }
